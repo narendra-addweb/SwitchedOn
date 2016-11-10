8db2d4d517619116f3321717f471cde2fba7c1fc
narendra.addweb <narendra.addweb@gmail.com>
2016-11-10 12:52:40 +0530

    Make changes as per ticket 78 - Blog New mockup

--- wp-content/themes/sapphire-wp/custom-blog-posts.php
+++ wp-content/themes/sapphire-wp/custom-blog-posts.php
@@ -1,4 +1,3 @@
-
 <?php 
 	/**
  * Template Name: Custom Blog Post
@@ -30,7 +29,9 @@ get_header(); ?>
 			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 			$args = array('post_type' => 'blogs','order' => 'DESC','pagination'=> true,'posts_per_page' => '2','paged' => $paged,'post_status'=> 'publish');
 			$custom_blog_post_data = get_posts($args);			
-		?><!-- **************************Custom Blog Post Data********************************** -->
+		?>
+
+		<!-- **************************Custom Blog Post Data********************************** -->
 		<div class="blog-posts wpb_column vc_column_container vc_col-sm-8">
 			<div class="blog-post-lists">
 				<h3 class="headline"><span>BLOGS</span></h3>
@@ -76,65 +77,7 @@ get_header(); ?>
 				//Pagination End
 			?></div>
 		</div>
-		<!-- **************************Related Blog Post <DATA></DATA>ata********************************** -->
-		<div class="blog-related-post wpb_column vc_column_container vc_col-sm-4">
-			<div class="blog-post-lists related-post">
-				<h3 class="headline"><span>RELATED BLOG</span></h3>
-				<?php
-					$chk_duplicate_key = array() ;
-					// print_r($custom_blog_post_data);
-					foreach ($custom_blog_post_data as $R_blogs_key => $R_blogs_value) {
-						$R_custom_field_data = get_post_custom($R_blogs_value->ID);
-						$objAuthor = get_user_by( 'ID', $R_blogs_value->post_author );
-						$post_author = $objAuthor->display_name;//Post author display name...
-						$serialize_post_data =  maybe_unserialize( $R_custom_field_data['crp_relations_to'][0] );
-						
-						if(!empty($serialize_post_data)) {
-							$related_blog_post = $serialize_post_data; 
-							//Get rendom two related post...
-							if(COUNT($serialize_post_data) > 2){
-								$random_blog_post = array_rand($serialize_post_data, 2);
-								$related_blog_post	= $random_blog_post;
-							}
-
-
-							foreach($serialize_post_data as $related_post_id => $r_post_data) { 
-								if(!in_array($related_post_id, $related_blog_post)) { 
-									$objPostData = $post = get_post( $related_post_id );
-									
-									//Get related post thumb in medium size...
-									$related_thumb = get_the_post_thumbnail( $related_post_id, 'medium' );
-									$postLink = get_post_permalink($related_post_id);//Post link...
-									$postTitle = $objPostData->post_title;//Post title...
-									$postPublishedDate = human_time_diff(get_post_time('U', false, $objPostData), current_time('timestamp'));//Post published date interval
-									$postAuthor = $post_author;//Post author...
-									?>
-									<div class="custom-blog-related-post blog-post-list">
-										<div class="blog-list-img"><?php 
-											echo '<a href="'.$postLink.'" title="'.$postTitle.'">';
-											echo $related_thumb;
-											echo '</a>';
-										?></div>
-										<div class="related-post-detail">
-											<div class="blog-list-author">
-												<h4><?php echo $postAuthor; ?> | <?php echo $postPublishedDate ." ".__('ago'); ?></h4>
-											</div>
-											<div class="blog-list-title">
-												<h3><a href="<?php echo $postLink;?>" title="<?php echo $postTitle;?>"><?php echo strtoupper($postTitle); ?></a></h3>
-											</div><?php
-											//Get the excerpt...
-											print get_excerpt_out_loop($related_post_id, 10);
-											?><div class="blog-list-desc">
-												<p><?php echo  $related_custom_post_excerpt; ?></p>
-											</div>
-										</div>
-									</div><?php
-					    	} 		
-							} 
-						}
-					} ?>
-				</div>
-			</div>
+		<!-- Remove 'Related Blog Post' code as per ticket #78 -->
 	</div>
 	
 </div>
