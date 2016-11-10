409837d02d2bd610ac40536a4c95264f95ec1a7d
narendra.addweb <narendra.addweb@gmail.com>
2016-11-10 15:48:57 +0530

    Structure changes for ticket 78 - Blog New mockup

--- wp-content/themes/sapphire-wp/custom-blog-posts.php
+++ wp-content/themes/sapphire-wp/custom-blog-posts.php
@@ -79,12 +79,7 @@ get_header(); ?>
 		</div>
 		<!-- Remove 'Related Blog Post' code as per ticket #78 -->
 		<div class="custom-blog-right-side"><?php 
-			while ( have_posts() ) : the_post(); ?> <!--Because the_content() works only inside a WP Loop -->
-      	<div class="entry-content-page">
-          <?php the_content(); ?> <!-- Page Content -->
-      	</div><!-- .entry-content-page --><?php
-	    endwhile; //resetting the page loop
-	    wp_reset_query(); 
+			dynamic_sidebar('custom-blogs');
 		?></div>
 	</div>
 	
