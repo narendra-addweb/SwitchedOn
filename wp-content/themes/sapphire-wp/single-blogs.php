646320e0f420fdfff9f8aa44ee3ee177b8213d21
narendra.addweb <narendra.addweb@gmail.com>
2016-11-11 12:45:49 +0530

    Add sidebar for post detail page where post type is blogs custom post

--- wp-content/themes/sapphire-wp/single-blogs.php
+++ wp-content/themes/sapphire-wp/single-blogs.php
@@ -34,20 +34,42 @@
 			
 	
 		case "layout-sidebar-single-right":
-			echo '<div class="ewf-row">';
-				echo '<div class="ewf-span8">';
-					
-					if ( have_posts() ) while ( have_posts() ) : the_post(); 
-						get_template_part('templates/custom-blog-default');
-					endwhile; 
+			if(is_singular('blogs')){
+				echo '<div class="ewf-row">';
+					echo '<div class="blog-posts wpb_column vc_column_container vc_col-sm-9 custom-blog-right-side">';
+						echo '<div class="blog-post-lists">';
+							if ( have_posts() ) while ( have_posts() ) : the_post(); 
+								get_template_part('templates/custom-blog-default');
+							endwhile; 
+						echo '</div>';
+					echo '</div>';
+					//Remove 'Related Blog Post' code as per ticket #78
+					echo '<div class="custom-blog-left-side vc_col-sm-3">';
+						dynamic_sidebar('custom-blogs');
+						echo '<div class="parent-custom-blog-side-1">';
+							dynamic_sidebar('custom-blog-side-1');
+						echo '</div>';
+						dynamic_sidebar('custom-blog-side-2');
+					echo '</div>';
+				echo '</div>';
+			}
+			else {
+				echo '<div class="ewf-row">';
+					echo '<div class="ewf-span8">';
 						
-				echo '</div>';
-				echo '<div class="ewf-span4">';
-				
-					dynamic_sidebar($page_blog_sidebar);
-					
-				echo '</div>';
-			echo '</div>';	
+						if ( have_posts() ) while ( have_posts() ) : the_post(); 
+							get_template_part('templates/custom-blog-default');
+						endwhile; 
+							
+					echo '</div>';
+					echo '<div class="ewf-span4">';
+						
+							dynamic_sidebar($page_blog_sidebar);
+						
+					echo '</div>';
+				echo '</div>';	
+			}
+			
 			break;
 	
 		case "layout-full": 
